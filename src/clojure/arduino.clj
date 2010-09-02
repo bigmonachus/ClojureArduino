0515ce3cc230f2369dec84b2abd34ee484b0ed10
Sergio Gonzalez <bigmonachus@gmail.com>
2010-09-02 15:15:55 -0500

    Communication works

--- src/clojure/arduino.clj
+++ src/clojure/arduino.clj
@@ -4,6 +4,15 @@
 	    [clojure.stacktrace :as ss])
   (:gen-class))
 
+(def raw-read (atom "")) ;;Points to data being read.
+
+(def accel (atom [0 0 0]))
+
+(defn parse-input
+  "Called by the event listener when a newline is found."
+  [s]
+  (println s))
+
 (defn get-port [^String name]
   (let [port-enum (CommPortIdentifier/getPortIdentifiers)
 	port-list (loop [p port-enum
@@ -11,9 +20,9 @@
 		    (if-not (. p hasMoreElements)
 		      list			     
 		      (recur p (conj list (. p nextElement)))))
-	port  (first (filter #(= (.getName %1) name) port-list))]
+	^CommPortIdentifier port-id  (first (filter #(= (.getName %1) name) port-list))]
     
-    (if-not port
+    (if-not port-id
       (do (println "Port" name "not found."
 		   "Here is a list of known ports:\n"
 		   "--------------------")
@@ -22,21 +31,35 @@
 	  (throw (new Exception "Port not found.")))      
       
       ;;    Else, Return new configured,open port      
-      (doto (.open port "Arduino" 100)
-	(. setSerialPortParams 9600 SerialPort/DATABITS_8
-	   SerialPort/STOPBITS_1
-	   SerialPort/PARITY_NONE)
-	(. addEventListener (proxy [gnu.io.SerialPortEventListener] []
-			      (serialEvent [^gnu.io.SerialPortEvent ev]
-					   (println "Serial event: " ev)
-					   (if (= ev SerialPortEvent/DATA_AVAILABLE)
-					     (println "data available")))))))))
+      (let [^SerialPort port (.open port-id "Arduino" 2000)
+	    listener (proxy [gnu.io.SerialPortEventListener
+			     Runnable] []
+		       (serialEvent [^gnu.io.SerialPortEvent ev]
+				    (when (= (.getEventType ev)
+					   SerialPortEvent/DATA_AVAILABLE)
+				      ;;read data available.
+				      (let [input-stream (.getInputStream port)
+					    char (char (.read input-stream))]
+					(swap! raw-read #(str % char))
+					(when (= \newline char)
+					  ;;call parse-input and reset raw-read
+					  (parse-input @raw-read)
+					  (swap! raw-read (fn [a] ""))
+					  )))]
+				      )))]
+	(doto port
+	  (. addEventListener listener)
+	  (. notifyOnDataAvailable true)	    
+	  (. setSerialPortParams 9600 SerialPort/DATABITS_8
+	     SerialPort/STOPBITS_1
+	     SerialPort/PARITY_NONE))))))
 
 (defn -main []
   (with-open
       [port (get-port "COM3")
        ]
-    (Thread/sleep 3000)
+    (prn port)
+    (Thread/sleep 5000)
     (println "Done.")
     ))
 
